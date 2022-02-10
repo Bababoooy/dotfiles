3ff96aca2b7f9998c2b22028ad0a8a569cee52dc
Bababoooy <86204625+Bababoooy@users.noreply.github.com>
2022-02-10 14:59:48 +0545

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,11 @@
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set fish_greeting
-neofetch 
+colorscript random
 
 starship init fish | source
+alias ls="lsd"
+alias cat="bat"
+export EDITOR='nvim'
