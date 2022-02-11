45a0c1b806507282962c4cba2d4f81c66a5d70bb
Bababoooy <86204625+Bababoooy@users.noreply.github.com>
2022-02-11 11:28:57 +0545

    Update battery.sh

--- Scripts/battery/battery.sh
+++ Scripts/battery/battery.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-battery_level=`cat /sys/class/power_supply/BAT0/capacity`
-if [ $battery_level -lt 15 ]
+
+battery_state=`acpi - b | grep "Battery 0" | awk '{print $3}' | sed 's/.$//'`
+battery_level=`acpi - b | grep "Battery 0" | awk -F "," '{print $2}' | sed 's/.$//'`
+
+if [[ $battery_state == "Discharging" ]]
 then
-  notify-send -h  string:fgcolor:#ff4444 -i ~/.scripts/battery/icon.png "Battery low" "Battery level is below 15%"
+  if [[ $battery_level < 15 ]]
+  then
+    notify-send -h  string:fgcolor:#ff4444 -i ~/.scripts/battery/icon.png "Battery low" "Battery level is${battery_level}%"
+  fi
 fi
+
 userid=$(id -u)
 DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$userid/bus"
 export DBUS_SESSION_BUS_ADDRESS
+
+## crontab -e command
+## " */5 * * * *  XDG_RUNTIME_DIR=/run/user/$(id -u) ~/.scripts/battery/battery.sh "
